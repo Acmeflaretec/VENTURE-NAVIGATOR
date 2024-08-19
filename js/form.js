c142825e46a6c146f2cb3b9ac607b1914272b6bb
Sourabh S <86180946+sourabhs2003@users.noreply.github.com>
2024-08-19 15:53:56 +0530

    updates last

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzPa8ITaTtJF_TRmDsnszyvzzeg7Iyep5uI6NadBTB0DY_Q_k7rvV8cY3P3ztPtr25H/exec';
-const form = document.forms['contact-form'];
-const submitButton = document.getElementById('submit');
+const scriptURL = 'https://script.google.com/macros/s/AKfycby3TztcSVzd-QyvSa86rhfa20iS1NOyaD3103uUn2fv0ePjgC-1KuX-Evyn5O5Lo68usw/exec';
+const form = document.forms['contact-form'];  // Update this if you have a different form name or ID
+const submitButton = document.querySelector('button[type="submit"]');  // Assuming there's only one submit button
 
 form.addEventListener('input', validateForm);
 form.addEventListener('submit', e => {
@@ -10,41 +10,19 @@ form.addEventListener('submit', e => {
         return;
     }
 
-    const phoneInput = document.getElementById('phone').value;
-    const phoneError = document.getElementById('phoneError');
-    const phonePattern = /^\d{10,}$/;
-
-    if (!phonePattern.test(phoneInput)) {
-        phoneError.textContent = "Please enter at least 10 digits.";
-        return;
-    }
-
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => alert("Thank you! Your form is submitted successfully."))
         .then(() => { window.location.reload(); })
         .catch(error => console.error('Error!', error.message));
 });
 
-document.getElementById('phone').addEventListener('input', function () {
-    var phone = this.value;
-    var phoneError = document.getElementById('phoneError');
-    var pattern = /^\d{10,}$/;
-
-    if (!pattern.test(phone)) {
-        phoneError.textContent = "Please enter at least 10 digits.";
-    } else {
-        phoneError.textContent = "";
-    }
-});
-
 function validateForm() {
-    const name = document.getElementById('name').value;
-    const phone = document.getElementById('phone').value;
-    const subject = document.getElementById('subject').value;
-    const message = document.getElementById('message').value;
-    const phonePattern = /^\d{10,}$/;
+    const companyName = document.getElementById('company-name').value;
+    const startupConcept = document.getElementById('startup-concept').value;
+    const currentStage = document.getElementById('current-stage').value;
+    const projectDifference = document.getElementById('project-difference').value;
 
-    if (name && phonePattern.test(phone) && subject && message) {
+    if (companyName && startupConcept && currentStage && projectDifference) {
         submitButton.disabled = false;
     } else {
         submitButton.disabled = true;
