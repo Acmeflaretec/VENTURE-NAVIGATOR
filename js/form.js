9d369fa93cfe3d928efcea93c6337df1491b97e4
Sourabh S <86180946+sourabhs2003@users.noreply.github.com>
2024-08-19 22:16:27 +0530

    updates fina

--- js/form.js
+++ js/form.js
@@ -1,6 +1,6 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycby3TztcSVzd-QyvSa86rhfa20iS1NOyaD3103uUn2fv0ePjgC-1KuX-Evyn5O5Lo68usw/exec';
-const form = document.forms['contact-form'];  // Update this if you have a different form name or ID
-const submitButton = document.querySelector('button[type="submit"]');  // Assuming there's only one submit button
+const form = document.forms['contact-form'];
+const submitButton = document.querySelector('button[type="submit"]');
 
 form.addEventListener('input', validateForm);
 form.addEventListener('submit', e => {
@@ -10,10 +10,33 @@ form.addEventListener('submit', e => {
         return;
     }
 
+    const whatsappInput = document.getElementById('whatsapp-number').value;
+    const whatsappError = document.getElementById('whatsappError');
+    const phonePattern = /^\d{10,}$/;
+
+    if (!phonePattern.test(whatsappInput)) {
+        whatsappError.textContent = "Please enter a valid WhatsApp number with at least 10 digits.";
+        return;
+    } else {
+        whatsappError.textContent = "";
+    }
+
     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
         .then(response => alert("Thank you! Your form is submitted successfully."))
         .then(() => { window.location.reload(); })
         .catch(error => console.error('Error!', error.message));
+});
+
+document.getElementById('whatsapp-number').addEventListener('input', function () {
+    var phone = this.value;
+    var whatsappError = document.getElementById('whatsappError');
+    var pattern = /^\d{10,}$/;
+
+    if (!pattern.test(phone)) {
+        whatsappError.textContent = "Please enter a valid WhatsApp number with at least 10 digits.";
+    } else {
+        whatsappError.textContent = "";
+    }
 });
 
 function validateForm() {
@@ -21,8 +44,10 @@ function validateForm() {
     const startupConcept = document.getElementById('startup-concept').value;
     const currentStage = document.getElementById('current-stage').value;
     const projectDifference = document.getElementById('project-difference').value;
+    const whatsappNumber = document.getElementById('whatsapp-number').value;
+    const phonePattern = /^\d{10,}$/;
 
-    if (companyName && startupConcept && currentStage && projectDifference) {
+    if (companyName && startupConcept && currentStage && projectDifference && phonePattern.test(whatsappNumber)) {
         submitButton.disabled = false;
     } else {
         submitButton.disabled = true;
